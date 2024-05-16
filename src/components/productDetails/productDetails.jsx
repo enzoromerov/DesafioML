23957b2ce16f6c8466a683603c495dfd00263677
mr0875 <enzoromerov@hotmail.com>
2024-05-16 05:23:19 -0300

    maqueta de la vista de detalle de producto

--- src/components/productDetails/productDetails.jsx
+++ src/components/productDetails/productDetails.jsx
@@ -2,13 +2,69 @@ import React from 'react';
 import './productDetails.scss';
 
 
-function productDetails () {
+function ProductDetails () {
   
   
   return (
-    <div>
+    <div className='productDetails'>
+      <div className='contenedorCategories'>
+            <div className='categories'>
+                <span><b>Apple</b>  Apple</span>
+            </div>
+      </div>
+      <div className='containerbody'>
+        <div className='row1product'>
+          <img src="" alt="" />
+          
+          <div className='block-Item'>
+          <div className='block-comprar'>
+            <div className='block-cantvendidos'>
+                <span >Nuevo</span> - <span>234 vendidos</span>
+                
+
+            </div>
+          </div>
+          <div className='titleItem'>
+            <span>Deco Reverse Sombrero Oxford</span>
+          </div>
+          <div className='priceItem'>
+            <span>$ 1.980</span>
+          </div>
+          <div className='button-comprar'>
+              <button >
+                <span>Comprar</span>
+                </button>
+          </div>
+          </div>
+
+        </div>
+
+        
+
+          <div className='title-descripcion'>
+            <span>Descripcion del producto</span>
+          </div>
+
+          <div className='detalle'>
+            <span>
+            Lorem ipsum dolor sit amet, consectetur
+             adipiscing elit. Cras mollis tellus non u
+             rna interdum venenatis. Duis mollis velit 
+             a lacus lobortis laoreet. Mauris leo tellus,
+              tempus eget augue eget, ornare pharetra lorem
+              . Nunc ut diam aliquet, mattis ex ullamcorper, co
+              nsectetur arcu. Class aptent taciti sociosqu ad litora 
+              torquent per conubia nostra, per inceptos himena
+              eos. Nulla ullamcorper sed risus quis varius. Vestib
+              ulum mi nunc, venenatis in leo in, ullamcorper 
+              mattis elit.
+            </span>
+          </div>
+
+        
+      </div>
      
     </div>
   );
 }
-export default productDetails;
+export default ProductDetails;
--- src/vistas/Buscar.jsx
+++ src/vistas/Buscar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import productDetails from '../components/productDetails/productDetails';
+import ProductDetails from '../components/productDetails/ProductDetails';
 import Resultados from '../components/resultados/Resultados';
 import E400 from '../components/errores/E400';
 import E404 from '../components/errores/E404';
@@ -22,7 +22,7 @@ const Buscar = () => {
             <Routes>
               <Route path="/*" />
               <Route path="/items?search=" element={<Resultados />} />
-              <Route path="/items/" element={<productDetails />} />              
+              <Route path="/items/" element={<ProductDetails />} />              
               <Route path="/400" element={<E400 />} />
               <Route path="/404" element={<E404 />} />
               <Route path="/408" element={<E408 />} />
